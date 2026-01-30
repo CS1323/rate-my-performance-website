dba7a9f278b124eee6111e5791ea90f28778aa5c
Cheyenne Sterbick <sterbickc@gmail.com>
2026-01-29 23:35:52 -0800

    app: make multiple pages using react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router';
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='cfu-boyfriend-quiz' element={<div>Test Quiz Page</div>} />
+      <Route path='about-me' element={<div>Test About Me Page</div>} />
+
+      <Route path='rules' element={<div>Test Rules Page</div>} />
+      <Route path='privacy-policy' element={<div>Test Privacy Policy Page</div>} />
+      <Route path='user-agreement' element={<div>Test User Agreement Page</div>} />
+      <Route path='accessibility' element={<div>Test Accessibility Page</div>} />
+      
+      <Route path='*' element={<div>Not Found</div>} />
+    </Routes>
   );
 }
 
