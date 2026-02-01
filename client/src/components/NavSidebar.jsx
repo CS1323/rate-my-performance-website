491f1d1789899ec2ee6498fbf4ac63a329a74bf7
Cheyenne Sterbick <sterbickc@gmail.com>
2026-01-31 21:01:54 -0800

    client: use Link for navigation

--- client/src/components/NavSidebar.jsx
+++ client/src/components/NavSidebar.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router';
+
+// Navigation Icons
 import HomeIcon from '../assets/images/icons/home.svg';
 import FileTextIcon from '../assets/images/icons/file-text.svg';
 import UserIcon from '../assets/images/icons/user.svg';
 import BookOpenIcon from '../assets/images/icons/book-open.svg';
 import ClipboardIcon from '../assets/images/icons/clipboard.svg';
+
 import './NavSidebar.css';
 
 export function NavSidebar() {
@@ -10,39 +14,39 @@ export function NavSidebar() {
     <nav className="left sidebar">
       {/* <!-- top section: navigation--> */}
       <div>
-        <div className="sidebar-link">
+        <Link to="/" className="sidebar-link">
           <img src={HomeIcon} />
             <div>Home</div>
-        </div>
-        <div className="sidebar-link">
+        </Link>
+        <Link to="/cfu-boyfriend-quiz" className="sidebar-link">
           <img src={FileTextIcon} />
             <div>CFU Boyfriend Quiz</div>
-        </div>
-        <div className="sidebar-link">
+        </Link>
+        <Link to="/about-me" className="sidebar-link">
           <img src={UserIcon} />
             <div>About Me</div>
-        </div>
+        </Link>
       </div>
       {/* <!-- bottom section: website compliance --> */}
       <div>
-        <div className="sidebar-link">
+        <Link to="/rules" className="sidebar-link">
           <img src={BookOpenIcon} />
             <div>Rules</div>
-        </div>
-        <div className="sidebar-link">
+        </Link>
+        <Link to="/privacy-policy" className="sidebar-link">
           <img src={ClipboardIcon} />
             <div>Privacy Policy</div>
-        </div>
-        <div className="sidebar-link">
+        </Link>
+        <Link to="/user-agreement" className="sidebar-link">
           <img src={ClipboardIcon} />
             <div>User Agreement</div>
-        </div>
-        <div className="sidebar-link">
+        </Link>
+        <Link to="/accessibility" className="sidebar-link">
           <span className="material-symbols-outlined">
             settings_accessibility
           </span>
           <div>Accessibility</div>
-        </div>
+        </Link>
         {/* <!-- copyright --> */}
         <p className="copyright">
           &copy; 2025 Cadence Keys
